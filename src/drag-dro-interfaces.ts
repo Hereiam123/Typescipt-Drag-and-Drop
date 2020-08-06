83f601f836c03c51077e4c84b941b692ef290090
Brian De Maio <bdemaio1@gmail.com>
2020-08-06 19:00:45 -0400

    WOrking on exporting from namespaces

---
+++ src/drag-dro-interfaces.ts
@@ -0,0 +1,15 @@
+namespace DDInterface {
+  //Drag and Drop Interfaces
+  //For draggable items
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  //Target for draggable item
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
